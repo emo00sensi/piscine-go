8cdd277f998be382fd68c46176634e497585b8b7
emo00sensi <mskini07xh@gmail.com>
2024-12-24 03:30:07 +0100

    nothing

---
+++ question 5/AlphaCount/AlphaCount.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AlphaCount(s string) int {
+	count := 0
+	for i:=0 ; i<len(s) ; i++{
+		if s[i]>='a' && s[i]<='z' {
+			 count++
+		}
+		if s[i]>='A' && s[i]<='Z' {
+			 count++
+		}
+	}
+	return count
+}
